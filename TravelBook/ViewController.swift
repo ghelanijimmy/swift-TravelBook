0b897e6911ea367df5f2fd92172ea2211439f0ad
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-18 08:14:28 -0400

    initial views

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var comment: UITextField!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = name.text
+            annotation.subtitle = comment.text
+            self.mapView.addAnnotation(annotation)
+        }
     }
 
-
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
+#Preview {
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    let controller = storyboard.instantiateViewController(withIdentifier: "mainVC")
+    
+//    return UINavigationController(rootViewController: controller)
+    return controller
+}
